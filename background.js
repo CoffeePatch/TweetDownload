e4573443a71f290cb7de2be9dad9e696c293f988
alonecoder-dev <teja.vallabhadasu@gmail.com>
2026-01-15 15:24:57 +0530

    refactor: Unminify and reformat CSS and JavaScript files for improved readability and update manifest and background scripts.

--- background.js
+++ background.js
@@ -1,1 +1,6 @@
-chrome.tabs.query({active:!0,lastFocusedWindow:!0},(function(n){n[0].url})),chrome.runtime.onInstalled.addListener((function(n){n.reason}));
+chrome.tabs.query({ active: !0, lastFocusedWindow: !0 }, function (n) {
+  n[0].url;
+}),
+  chrome.runtime.onInstalled.addListener(function (n) {
+    n.reason;
+  });
